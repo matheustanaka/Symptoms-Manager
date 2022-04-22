b91dbbcf4467409c33b79e636527792c04c63492
matheustanaka <matheustanakanog@gmail.com>
2022-04-22 14:14:09 -0300

    feat: add button inside dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./style";
+import { ButtonCreate } from "../Button";
+
+export function Dashboard() {
+    return (
+        <Container>
+                <ButtonCreate />
+        </Container>
+    );
+}
