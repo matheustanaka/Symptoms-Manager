9fd77c4b400fbc9320bf2ec33f28f9b5044b1946
matheustanaka <matheustanakanog@gmail.com>
2022-04-23 22:19:46 -0300

    feat: add headers to user api

--- src/services/UserApi.ts
+++ src/services/UserApi.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const UserApi = axios.create({
-    baseURL: "http://localhost:3333/users",
+    baseURL: "http://localhost:3333",
+    headers: {
+        "Content-type": "application/json"
+    } 
 })
