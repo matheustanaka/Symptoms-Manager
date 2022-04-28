d24da4a6c4a5f689d895f7d1a1f8d2bf4ddfa189
matheustanaka <matheustanakanog@gmail.com>
2022-04-28 19:45:08 -0300

    feat: add useUser hook

---
+++ src/hooks/useUsers.tsx
@@ -0,0 +1,53 @@
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
+import { UserApi } from "../services/UserApi";
+import { UserType } from "../types/UserType";
+
+type UserInput = Omit<UserType, 'id'>;
+
+interface UsersProviderProps {
+    children: ReactNode;
+}
+
+interface UsersContextData {
+    users: UserType[];
+    createUser: (user: UserInput) => Promise<void>;
+}
+
+const UsersContext = createContext<UsersContextData>({} as UsersContextData);
+
+export function UsersProvider ({ children }: UsersProviderProps) {
+    const [users, setUser] = useState<UserType[]>([]);
+
+    useEffect(() => {
+        UserApi.get("/users")
+        .then((response) => setUser(response.data))
+        .catch((err) => {
+            console.log("Evita dog " + err)
+        })
+    }, []);
+
+    async function createUser(userInput: UserInput) {
+        const response = await UserApi.post('/users', {
+            ...userInput,
+        })
+
+        const { user } = response.data;
+
+        setUser([
+            ...users,
+            user,
+        ])
+
+    }
+
+    return (
+        <UsersContext.Provider value={{ users, createUser}}>
+            {children}
+        </UsersContext.Provider>
+    )
+}
+
+export function useUsers() {
+    const context = useContext(UsersContext);
+    return context;
+}
