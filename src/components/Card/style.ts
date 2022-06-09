6e87aae7d26faa5df141cf139f1bf5a135b18ec3
matheustanaka <matheustanakanog@gmail.com>
2022-06-09 13:39:33 -0300

    style: remake card box css

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,20 +1,14 @@
 import styled from "styled-components";
 export const Container = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 3rem;
-    
+    padding: 5px;
 
-
-    .box-card {
+    .card-box {
         display: flex;
         align-items: center;
         flex-direction: column;
         padding: 2rem;
         background: var(--box);
         border-radius: 1rem;
-
-        margin-top: 1.5rem;
     }
 
 `;
