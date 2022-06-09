7d7e1c93a0f1b75f0d65e4dd5bce30977ac12ccf
matheustanaka <matheustanakanog@gmail.com>
2022-06-09 13:39:59 -0300

    style: add grid template to main content

--- src/components/Dashboard/style.ts
+++ src/components/Dashboard/style.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+
     max-width: 1120px;
     margin: 0 auto ;
     padding: 2.5rem;
