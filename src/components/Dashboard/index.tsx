2b99f1d1eee268bc9da04d4d3a257b5789577979
matheustanaka <matheustanakanog@gmail.com>
2022-06-09 13:42:03 -0300

    refactor: map card component inside dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,14 @@
 import { Container } from "./style";
 import { Card } from "../Card";
-
+import { useUsers } from "../../hooks/useUsers";
 
 export function Dashboard() {
+    const { users } = useUsers();
     return (
         <Container>
-                <Card />
+            {users.map((user) => (
+                <Card key={user._id} user={user} />
+            ))}
         </Container>
     );
 }
