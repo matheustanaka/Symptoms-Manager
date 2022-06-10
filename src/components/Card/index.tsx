7f8febdd8b76a3afec67544f9389659569880715
matheustanaka <matheustanakanog@gmail.com>
2022-06-10 16:36:04 -0300

    feat: add button to edit and delete

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
-
+import { TiDelete } from "@react-icons/all-files/ti/TiDelete";
+import { FiEdit } from "@react-icons/all-files/fi/FiEdit"
 import { Container } from "./style";
 
 interface UserProps {
@@ -13,9 +14,19 @@ export function Card(props: UserProps) {
     return (
         <Container>
             <div className="card-box">
-                <h2>{props.user.name}</h2>
-                <h3>{props.user.email}</h3>
-                <p>{props.user.symptoms}</p>
+                <div className="icons">
+                    <FiEdit size={23}
+                        color="#00a1ff"
+                    />
+                    <TiDelete size={25}
+                        color="#F97474"
+                    />
+                </div>
+                <div className="user-info">
+                    <h2>{props.user.name}</h2>
+                    <h3>{props.user.email}</h3>
+                    <p>{props.user.symptoms}</p>
+                </div>
             </div>
         </Container>
     );
--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -3,12 +3,21 @@ export const Container = styled.div`
     padding: 5px;
 
     .card-box {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        padding: 2rem;
+        padding: 1.5rem;
         background: var(--box);
         border-radius: 1rem;
+        
+        .icons {
+            display: flex;
+            justify-content: flex-end;
+            cursor: pointer;
+        }
+
+        .user-info {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+        }
     }
 
 `;
